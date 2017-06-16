b88155fd543857664969982102819557cd375fa6
NikolaySapundzhiev <onilo1@abv.bg>
2017-06-16 14:57:54 +0300

    Fix out of memory exception

--- src/Worker.java
+++ src/Worker.java
@@ -1,15 +1,23 @@
-import java.nio.MappedByteBuffer;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Worker implements Runnable {
-    private MappedByteBuffer buffer;
+    private File file;
     private Map<Character, Integer> map;
     private long start;
     private long end;
 
-    public Worker(MappedByteBuffer buffer, long start, long end) {
-        this.buffer = buffer;
+    public Worker(File file, long start, long end) {
+        this.file = file;
         this.start = start;
         this.end = end;
         this.map = new HashMap<>();
@@ -17,18 +25,46 @@ public class Worker implements Runnable {
 
     @Override
     public void run() {
-        for (long i = start; i < end; i++) {
-            byte b = buffer.get();
-            char ch = (char) b;
-
-            if (!map.containsKey(ch)) {
-                map.put(ch, 1);
-            } else {
-                int n = map.get(ch);
-                map.put(ch, ++n);
-            }
+        long bufferLength = end - start;
+        InputStream inputStream = null;
+        
+        try {
+            inputStream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-
-//        System.out.println(map.toString());
+        
+        Reader reader = new InputStreamReader(inputStream, Charset.defaultCharset());
+        Reader buffer = new BufferedReader(reader);
+        
+        try {
+            buffer.skip(start);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        for (long i = 0; i < bufferLenght; i++) {
+            int intCharacter = 0;
+            
+            try {
+                intCharacter = buffer.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            char character = (char) intCharacter;
+            collectDataInMap(character);
+        }
+        
+        System.out.println(map.toString());
+    }    
+    
+    private void collectDataInMap(char character) {        
+        if (!map.containsKey(character)) {
+            map.put(character, 1);
+        } else {
+            int n = map.get(character);
+            map.put(character, ++n);
+        }
     }
 }
